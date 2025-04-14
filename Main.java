bfbfc7b13c1624a68f1f1885fefebe62a4733633
kaan aydın <101866448+Aydinkaan@users.noreply.github.com>
2025-04-14 16:45:03 +0300

    Asal Sayı Bulma
    Asal Sayı Bulma

--- Main.java
+++ Main.java
@@ -1,26 +1,30 @@
-//basamak değeri bulmak için do-while kullanabilirsin
+// Asal sayı bulma
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Sayıyı girin:");
+        System.out.println("Sayı giriniz:");
         int sayi = scanner.nextInt();
-        int basamak_sayisi = String.valueOf(sayi).length();
-        int gecici = sayi;
-        int toplam = 0;
-        do {
-            int basamak_degeri = gecici % 10;
-            gecici /= 10;
-            toplam += Math.pow(basamak_degeri,basamak_sayisi);
 
-        }
-        while  (gecici > 0);
-            if (sayi == toplam){
-                System.out.println("Bu bir armstrong sayısıdır: " + sayi);
-        }
-            else {
-                System.out.println("Armstrong sayısı değildir.");
+        boolean asal = true; // tanımladık
 
+        if (sayi < 2) {
+            asal = false;
         }
+        else {
+            for (int i = 2; i<sayi;i++){ // Sayıya kadar olan tüm sayı değerlerine git ve sayıyı o değerlere böl
+                if(sayi % i ==0){
+                    asal = false;
+                    break;
+                }
+            }
+        }
+        if (asal){
+            System.out.println("Bu sayı asal sayıdır: " + sayi);
+        }
+        else {
+            System.out.println("Bu sayı asal değildir..");
+        }
+
     }
 }
